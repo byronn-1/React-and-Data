c6dc2952e07961fddedd9df6e76c5a040e127578
Byron Lee <byronn.lee@gmail.com>
2022-11-11 06:35:08 +0000

    added navigation away from form with useNav hook

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,17 +1,22 @@
+
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const API_ENDPOINT = process.env.REACT_APP_VARIABLE_FIREBASE_ENDPOINT;
 
 export default function NewMeetupsPage() {
-    
+    let navigate = useNavigate();
+
     function addMeetupHandler(meetupData) { 
         fetch(API_ENDPOINT, {
             method: 'POST',
             body: JSON.stringify(meetupData),
-            hearders: {
+            headers: {
                 'Content-Type': 'application/json'
             }
-        });
+        }).then(() => {
+            navigate.replace('/');
+        })
     }
     
     return (
